e442bd876fabaa9b6f661e1c3357f3e6ade7171c
Eduard0Lopes <luizeduardolopes2001@gmail.com>
2026-09-03 13:59:51 -0300

    feat/arquivo main do trabalho

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int alturaQueda = 20;
+        int alturaMortal = 20;
+        boolean caiuNaAgua = true;
+
+        if (caiuNaAgua) {
+            System.out.println("Sobreviveu na água!");
+        } else if (alturaQueda >= alturaMortal) {
+            System.out.println("Voce morreu!");
+        } else {
+            System.out.println("Levou dano de queda");
+        }
+    }
+}
